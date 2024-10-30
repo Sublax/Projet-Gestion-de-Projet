3bf84082aaee7021c7744c4954d396e2b71afc7b
Corentin <corentin.labat-jarleton@etu.univ-montp3.fr>
2024-10-30 10:43:18 +0100

    Ajout et tests réussis sur la partie php de page Forum et Commentaires

--- pageweb/forum/commentaires.php
+++ pageweb/forum/commentaires.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include "../bd.php";
 $bdd = getBD();
 if(isset($_GET['id_pays'])){
@@ -14,7 +15,9 @@ if(isset($_GET['id_pays'])){
         echo 'ID Incorrect';
         exit;
     }
-
+}else{
+    die("Erreur d'ID");
+    exit();
 }
 ?>
 
@@ -28,9 +31,46 @@ if(isset($_GET['id_pays'])){
 </head>
 <body>
 <section class="forum">
-<h1> Bienvenue sur le forum <?php echo '' . $nom_pays .'' ?> </h1>
-<input id="comments" placeholder="Ajoutez votre commentaire"> 
+<h1> Bienvenue sur le forum <?php echo '' . $nom_pays .'' ?> </h1>   
+    <?php
+    // -- Partie Affichage du formulaire
+    if(isset($_SESSION['id_client'])){
+        $id_client = $_SESSION['id_client'];
+        echo '<form action="" method="POST">
+        <input type="text" id="commentaire" name="commentaire" placeholder="Ajoutez votre commentaire">
+        <button type="submit">Envoyer</button>
+        </form>';
+    }else{
+        echo 'Vous devez être connecté pour poster un commentaire !';
+    }
 
+    // -- Partie Insertion base de données
+    if(isset($_POST["commentaire"])){
+        $commentaire = htmlspecialchars($_POST['commentaire']);
+        $stmt = $bdd->prepare("INSERT INTO avis (id_client,id_pays,avis,date) VALUES (:id_client,:id_pays,:avis,NOW());");
+        $stmt->execute([
+            ':id_client' => $id_client,
+            ':id_pays' => $id_pays,
+            ':avis' => $commentaire
+        ]);
+        echo '<p> Votre commentaire a été ajouté avec succès !</p>';
+    }
+
+
+    // -- Partie Affichage des commentaires de la bdd :
+    $stmt = $bdd -> prepare('SELECT avis.*, clients.nom_utilisateur, pays.id_pays FROM avis INNER JOIN clients ON clients.id_client = avis.id_client INNER JOIN pays ON pays.id_pays = avis.id_pays WHERE pays.id_pays = :id_pays ORDER BY id_avis DESC;');
+    $stmt->execute([':id_pays' => $id_pays]);
+    if ($stmt->rowCount() == 0) {
+        echo '<p> Aucun commentaire sur ce pays pour le moment ! :)</p>';
+    }else{
+        while($ligne = $stmt->fetch()){
+            echo '<p> '. $ligne['date'] .' Commentaire écrit par '.$ligne['nom_utilisateur'] . ' :</p>';
+            echo '<p>' .$ligne["avis"] . '</p>';
+            echo '<br> <br>';
+        }
+    }
+
+    ?>
 </section>
 </body>
 </html>
