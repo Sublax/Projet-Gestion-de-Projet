223cec6e1f7ecaaad3780bf35499975656ef05ef
Ruiz-Maximiliano <maximiliano-benjamin.ruiz-zamorano@etu.univ-montp3.fr>
2024-10-31 10:18:01 +0100

    Réorganisation de fichiers dans la structure de pageweb

--- pageweb/connexion/process_register.php
+++ pageweb/connexion/process_register.php
@@ -13,20 +13,30 @@ $mail = $_POST['email'];
 $localisation = $_POST['location'];
 $mdp = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hachage du mot de passe
 
-// Préparer la requête SQL pour insérer les données dans la table clients
-$sql = "INSERT INTO clients (nom_utilisateur, nom, prenom, mail, localisation, mdp) VALUES (?, ?, ?, ?, ?, ?)";
+// Vérifier si le nom_utilisateur ou le mail existe déjà
+$sql_check = "SELECT COUNT(*) FROM clients WHERE nom_utilisateur = ? OR mail = ?";
+$stmt_check = $conn->prepare($sql_check);
+$stmt_check->execute([$nom_utilisateur, $mail]);
+$existingUser = $stmt_check->fetchColumn();
 
-// Utiliser une requête préparée pour éviter les injections SQL
-$stmt = $conn->prepare($sql);
-$stmt->execute([$nom_utilisateur, $nom, $prenom, $mail, $localisation, $mdp]);
-
-// Vérifier si l'insertion a réussi
-if ($stmt) {
-    echo "Inscription réussie !";
-    header("Location: login.php"); // Rediriger vers la page de connexion après l'inscription
-    exit();
+if ($existingUser > 0) {
+    echo "Nom d'utilisateur ou email déjà utilisé. Veuillez en choisir un autre.";
 } else {
-    echo "Erreur lors de l'inscription.";
+    // Préparer la requête SQL pour insérer les données dans la table clients
+    $sql = "INSERT INTO clients (nom_utilisateur, nom, prenom, mail, localisation, mdp) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Utiliser une requête préparée pour éviter les injections SQL
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$nom_utilisateur, $nom, $prenom, $mail, $localisation, $mdp]);
+
+    // Vérifier si l'insertion a réussi
+    if ($stmt) {
+        echo "Inscription réussie !";
+        header("Location: login.php"); // Rediriger vers la page de connexion après l'inscription
+        exit();
+    } else {
+        echo "Erreur lors de l'inscription.";
+    }
 }
 
 // Fermer la connexion
