c8fe0f54bf445bcf0106ef21f68d70b9c65982c3
Ruiz-Maximiliano <maximiliano-benjamin.ruiz-zamorano@etu.univ-montp3.fr>
2024-11-14 19:30:12 +0100

    reglage balise a

--- pageweb/forum/forum.php
+++ pageweb/forum/forum.php
@@ -20,15 +20,17 @@ $bdd = getBD();
     <div class="menu-item">
     <?php
         if (isset($_SESSION['client'])) {
-            echo '<a href="../questionnaire.php"</a>';
+            echo '<a href="../questionnaire.php">';
         } else {
-            echo '<a href="../connexion/login.php" </a>';
+            echo '<a href="../connexion/login.php">';
         }
-        ?><img src="../images/images_ced/icone1.png" alt="Icone Questionnaire">
+        ?>
+        <img src="../images/images_ced/icone1.png" alt="Icone Questionnaire">
+        </a>
         <p>Questionnaire</p>
     </div>
     <div class="menu-item">
-    <a href="graph.php"><img src="../images/images_ced/icone2.png" alt="Icone Statistiques & Graphs">
+    <a href="graph.php"><img src="../images/images_ced/icone2.png" alt="Icone Statistiques & Graphs"></a>
         <p>Statistiques & Graphs</p>
     </div>
     <div class="menu-item">
@@ -36,19 +38,19 @@ $bdd = getBD();
        <p>Forum</p>
    </div>
     <div class="menu-item logo">
-    <a href="../index.php"><img src="../images/images_ced/icone3.png" alt="Logo">
+    <a href="../index.php"><img src="../images/images_ced/icone3.png" alt="Logo"></a>
         
     </div>
     <div class="menu-item">
-    <a href="../informations/informations.php"><img src="../images/images_ced/icone4.png" alt="Icone Informations">
+    <a href="../informations/informations.php"><img src="../images/images_ced/icone4.png" alt="Icone Informations"></a>
         <p>Informations</p>
     </div>
     <div class="menu-item">
-    <a href="../informations/sources.php"><img src="../images/images_ced/icone5.png" alt="Icone Sources données">
+    <a href="../informations/sources.php"><img src="../images/images_ced/icone5.png" alt="Icone Sources données"></a>
         <p>Sources données</p>
     </div>
     <div class="menu-item">
-    <a href="../profil.php"><img src="../images/images_ced/icone6.png" alt="Icone Options">
+    <a href="../profil.php"><img src="../images/images_ced/icone6.png" alt="Icone Options"></a>
         <p>Profil</p>
     </div>
     </header>
--- pageweb/informations/sources.php
+++ pageweb/informations/sources.php
@@ -12,16 +12,18 @@
             <div class="menu-bar">
             <div class="menu-item">
             <?php
-                if (isset($_SESSION['client'])) {
-                    echo '<a href="../questionnaire.php"</a>';
-                } else {
-                    echo '<a href="../connexion/login.php" </a>';
-                }
-                ?><img src="../images/images_ced/icone1.png" alt="Icone Questionnaire">
+            if (isset($_SESSION['client'])) {
+                echo '<a href="../questionnaire.php">';
+            } else {
+                echo '<a href="../connexion/login.php">';
+            }
+            ?>
+            <img src="../images/images_ced/icone1.png" alt="Icone Questionnaire">
+            </a>
                 <p>Questionnaire</p>
             </div>
             <div class="menu-item">
-            <a href="../graph.php"><img src="../images/images_ced/icone2.png" alt="Icone Statistiques & Graphs">
+            <a href="../graph.php"><img src="../images/images_ced/icone2.png" alt="Icone Statistiques & Graphs"></a>
                 <p>Statistiques & Graphs</p>
             </div>
             <div class="menu-item">
@@ -29,19 +31,19 @@
                <p>Forum</p>
            </div>
             <div class="menu-item logo">
-            <a href="../index.php"><img src="../images/images_ced/icone3.png" alt="Logo">
+            <a href="../index.php"><img src="../images/images_ced/icone3.png" alt="Logo"></a>
                 
             </div>
             <div class="menu-item">
-            <a href="informations.php"><img src="../images/images_ced/icone4.png" alt="Icone Informations">
+            <a href="informations.php"><img src="../images/images_ced/icone4.png" alt="Icone Informations"></a>
                 <p>Informations</p>
             </div>
             <div class="menu-item">
-            <a href="sources.php"><img src="../images/images_ced/icone5.png" alt="Icone Sources données">
+            <a href="sources.php"><img src="../images/images_ced/icone5.png" alt="Icone Sources données"></a>
                 <p>Sources données</p>
             </div>
             <div class="menu-item">
-            <a href="../profil.php"><img src="../images/images_ced/icone6.png" alt="Icone Options">
+            <a href="../profil.php"><img src="../images/images_ced/icone6.png" alt="Icone Options"></a>
                 <p>Profil</p>
             </div>
             </header>
