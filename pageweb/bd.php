01da515c228d102664d75d4c7465279ff51a96d6
Bogdan981011 <122132248+Bogdan981011@users.noreply.github.com>
2025-02-05 10:22:20 +0100

    Add files via upload

--- pageweb/bd.php
+++ pageweb/bd.php
@@ -4,4 +4,25 @@ function getBD(){
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $bdd;
 }
+
+function getData($bdd, $table){
+        if ($table == 'economie'){
+                $stmt = $bdd->prepare("SELECT * 
+                                FROM $table, pays
+                                WHERE $table.id_country = pays.id_pays");
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }elseif ($table == 'pays'){
+                $stmt = $bdd->prepare("SELECT * 
+                                FROM $table");
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+                $stmt = $bdd->prepare("SELECT * 
+                                FROM $table, pays
+                                WHERE $table.id_pays = pays.id_pays");
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+}
 ?>
