b6613470492fc8e2ccebf762392fc8a65b29c2df
Ruiz-Maximiliano <maximiliano-benjamin.ruiz-zamorano@etu.univ-montp3.fr>
2025-04-09 08:30:17 +0200

    test hebergement chat-ia.js

--- pageweb/js/chat.js
+++ pageweb/js/chat.js
@@ -1,4 +1,4 @@
-const res = await fetch('http://localhost:3000/api/chat', {
+const res = await fetch('projet-gestion-de-projet-production-f29a.up.railway.app', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ message })
