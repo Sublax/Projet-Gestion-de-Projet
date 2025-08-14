1a6a5dc34e0b7cc92fa54dc5e0f028328da7a129
Bogdan981011 <122132248+Bogdan981011@users.noreply.github.com>
2025-08-14 17:10:35 +0300

    Update bd.php

--- pageweb/bd.php
+++ pageweb/bd.php
@@ -1,7 +1,27 @@
 <?php
 function getBD(){ 
-  $bdd = new PDO('mysql:host=localhost;dbname=bdprojet;charset=utf8', 'root', 'root'); 
-  return $bdd; 
+  function getBD(): PDO {
+  $url = getenv('DATABASE_URL');
+  if (!$url) {
+    throw new RuntimeException('DATABASE_URL not set');
+  }
+  $p = parse_url($url);
+  if (!isset($p['host'], $p['user'], $p['pass'], $p['path'])) {
+    throw new RuntimeException('Invalid DATABASE_URL');
+  }
+
+  $host = $p['host'];
+  $port = $p['port'] ?? 3306;
+  $user = $p['user'];
+  $pass = $p['pass'];
+  $db   = ltrim($p['path'], '/');
+
+  $dsn = "mysql:host={$host};port={$port};dbname={$db};charset=utf8mb4";
+  return new PDO($dsn, $user, $pass, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+  ]); 
 }
 
 function getData($bdd, $table){
